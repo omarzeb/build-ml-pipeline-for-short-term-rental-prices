211ae1fb62256f38ec4e6a915a4706b127d9c73a
Omar Zeb <omar_zeb@hotmail.com>
2022-07-25 22:45:23 +0500

    added lat long boundries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,6 +30,9 @@ def go(args):
     idx = df['price'].between(price_min, price_max)
     df = df[idx].copy()
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Saving the clean data")
     df.to_csv("clean_data.csv", index=False)
 
